81e074ee73ba52356af01daca9184d6ca23c3171
Paul Edenburg <paul@redloyalty.nl>
2017-08-27 12:44:16 +0200

    Changed the size for db-columns to 255 as this takes just 1byte instead of 2 bytes when 256 is used

--- resources/schema.sql
+++ resources/schema.sql
@@ -1,21 +1,21 @@
   CREATE TABLE `websites` (
-	`id` VARCHAR(256) NOT NULL,
+	`id` VARCHAR(255) NOT NULL,
 	`url` TEXT(2083) NOT NULL,
-	`url_hash` VARCHAR(256) NOT NULL,
+	`url_hash` VARCHAR(255) NOT NULL,
 	`name` VARCHAR(50) NOT NULL UNIQUE,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `endpoints` (
-	`id` VARCHAR(256) NOT NULL,
-	`website_id` VARCHAR(256) NOT NULL,
+	`id` VARCHAR(255) NOT NULL,
+	`website_id` VARCHAR(255) NOT NULL,
 	`name` VARCHAR(50) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `selectors` (
-	`id` VARCHAR(256) NOT NULL,
-	`endpoint_id` VARCHAR(256) NOT NULL,
+	`id` VARCHAR(255) NOT NULL,
+	`endpoint_id` VARCHAR(255) NOT NULL,
 	`selector` VARCHAR(150) NOT NULL,
 	`alias` VARCHAR(150) NOT NULL,
 	PRIMARY KEY (`id`)
